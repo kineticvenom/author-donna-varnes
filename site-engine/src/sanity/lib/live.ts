71cd28b8f77101f28b59ba33172124046b9ee928
Kaleb Varnes <kalebvarnes@yahoo.com>
2025-03-23 16:02:55 -1000

    removed all

--- site-engine/src/sanity/lib/live.ts
+++
@@ -1,9 +0,0 @@
-import { client } from "./client";
-import { defineLive } from "next-sanity";
-import { token } from "@/sanity/lib/token";
-
-export const { sanityFetch, SanityLive } = defineLive({
-  client: client,
-  serverToken: token,
-  browserToken: token,
-});
