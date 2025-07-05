873fd11f3c776eab34dd71c9708c2b7a07f71ab9
Kaleb Varnes <kalebvarnes@yahoo.com>
2025-07-04 17:19:03 -1000

    removed codegen stuff

--- author/sanity-codegen.config.js
+++
@@ -1,7 +0,0 @@
-// sanity-codegen.config.js
-module.exports = {
-  schemaPath: './studio/src/schemaTypes/index.ts', // adjust as needed
-  outputPath: './nextjs-app/sanity.codegentypes.ts',
-  dataset: 'production',
-  projectId: '7llz646c', // ← your actual Sanity project ID
-};
