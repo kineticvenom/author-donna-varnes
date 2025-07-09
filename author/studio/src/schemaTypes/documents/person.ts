f60da759e890fa6be7d8d190eed0d15de8c23e5d
Kaleb Varnes <kalebvarnes@yahoo.com>
2025-07-08 18:07:23 -1000

    Removed required Alt text for image on person

--- author/studio/src/schemaTypes/documents/person.ts
+++ author/studio/src/schemaTypes/documents/person.ts
@@ -34,15 +34,15 @@ export const person = defineType({
           type: 'string',
           title: 'Alternative text',
           description: 'Important for SEO and accessibility.',
-          validation: (rule) => {
-            // Custom validation to ensure alt text is provided if the image is present. https://www.sanity.io/docs/validation
-            return rule.custom((alt, context) => {
-              if ((context.document?.picture as any)?.asset?._ref && !alt) {
-                return 'Required'
-              }
-              return true
-            })
-          },
+          // validation: (rule) => {
+          //   // Custom validation to ensure alt text is provided if the image is present. https://www.sanity.io/docs/validation
+          //   return rule.custom((alt, context) => {
+          //     if ((context.document?.picture as any)?.asset?._ref && !alt) {
+          //       return 'Required'
+          //     }
+          //     return true
+          //   })
+          // },
         }),
       ],
       options: {
